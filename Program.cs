d15f363cc8401894fd9be62fdfa0207fdc0a78f7
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-15 17:25:53 +0800

    Решила задачу 27

--- Program.cs
+++ Program.cs
@@ -3,3 +3,22 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+int SumNumbers(int a)
+{
+    int sum = 0;
+    if (a == 0)
+    {
+        return 0;
+    }
+    while (a != 0)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+    
+}
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumbers(A));
